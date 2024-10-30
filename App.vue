05e1d45c0e701a456ef97511940cd098a2b7a5b8
breann0ng <breann.ong.2023@scis.smu.edu.sg>
2024-10-30 12:53:29 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import login from './components/login.vue'
+// import register from './components/register.vue'
+import Carousel from './components/Carousel.vue';
+
+</script>
+
+<template>
+    <div id="app">
+        <router-view></router-view> <!-- Renders the current route component -->
+    </div>
+    <main>
+        <TheWelcome />
+    </main>
+</template>
+
+<template>
+    <div id="app">
+        <Carousel />
+    </div>
+</template>
