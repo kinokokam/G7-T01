96e907537ef502bb124faabb71db642f63eabfa7
breann0ng <breann.ong.2023@scis.smu.edu.sg>
2024-10-30 12:53:16 +0800

    Update explore.vue

--- explore.vue
+++ explore.vue
@@ -52,10 +52,7 @@
                     <b>Email:</b> {{ item.email }} <br />
                     <b>Contact info:</b> {{ item.contact }}
                 </p>
-<<<<<<< Updated upstream
                 <button @click="saveToFavorites(item)" class="btn btn-primary">Save to Favorites</button>
-=======
->>>>>>> Stashed changes
             </div>
         </div>
 
@@ -70,7 +67,6 @@
 
 <script>
 import { db } from '../path/to/firebase';
-<<<<<<< Updated upstream
 import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
@@ -163,8 +159,6 @@ export default {
 
 <script>
 import { db } from '../path/to/firebase';
-=======
->>>>>>> Stashed changes
 import { collection, getDocs } from 'firebase/firestore';
 
 export default {
